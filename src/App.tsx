844d65cbe279c15064049f1624b0b8ab52bc328d
Nicholas <urkona@list.ru>
2024-07-07 18:50:29 +0300

    feat: add Husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
+  const TestList = () => {
+    const items = ['Item 1', 'Item 2', 'Item 3'];
+    return (
+      <ul>
+        {items.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    );
+  };
   return (
     <>
       <div>
@@ -16,20 +25,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>Vite + React</h1><div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div><p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
