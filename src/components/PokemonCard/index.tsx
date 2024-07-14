e973cff3edb17f0749ad9f3542cf3706e86332c3
Nicholas <urkona@list.ru>
2024-07-15 00:31:51 +0300

    feat: update to functional components

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Pokemon } from '../../services/types/api.interface';
+import { Link, useLocation } from 'react-router-dom';
 import './style.scss';
 
-interface PokemonCardProps {
-  pokemon: Pokemon;
+export interface PokemonProps {
+  name: string;
+  sprites: string;
+  url: string;
 }
 
-const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
+export const PokemonCard = (props: PokemonProps) => {
+  const { pathname } = useLocation();
+  const { name, url, sprites } = props;
+  console.log(name, url, sprites);
+  const pokemonId = url.split('/').reverse()[1];
+
   return (
+    // <div className="pokemon-card">
+    //   <h3>{name}</h3>
+    //   <div>
+    //     {/* <ul>
+    //       <li>url: {this.pokemon.url}</li>
+    //     </ul> */}
+    //     <img src={sprites} alt={name} />
+    //   </div>
+    // </div>
     <div className="pokemon-card">
-      <h3>{pokemon.name}</h3>
-      <img src={pokemon.sprites} alt={pokemon.name} />
+      <Link
+        to={
+          `/pokemon/${pokemonId}` === pathname ? '/' : `/pokemon/${pokemonId}`
+        }
+        className="pokemon"
+      >
+        <h3>{name}</h3>
+        <img src={sprites} alt={name} />
+      </Link>
     </div>
   );
 };
-
-export default PokemonCard;
