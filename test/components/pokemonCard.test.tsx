b27215b24555b49e89c35180fac2c71653a24e58
Nicholas <urkona@list.ru>
2024-07-15 02:28:42 +0300

    feat: add test

--- test/components/pokemonCard.test.tsx
+++ test/components/pokemonCard.test.tsx
@@ -2,13 +2,14 @@ import { render, screen } from '@testing-library/react';
 import { vi, expect } from 'vitest';
 import '@testing-library/jest-dom';
 import React from 'react';
-import { PokemonCard,PokemonProps } from '../../src/components/pokemonCard';
+import { PokemonCard, PokemonProps } from '../../src/components/pokemonCard';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('check PokemonCard', () => {
   const mockProps: PokemonProps = {
     name: 'pikachu',
-    sprites: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+    sprites:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
     url: 'https://pokeapi.co/api/v2/pokemon/pikachu',
   };
 
@@ -18,7 +19,10 @@ describe('check PokemonCard', () => {
         <PokemonCard {...mockProps} />
       </MemoryRouter>
     );
-    expect(screen.getByAltText('pikachu')).toHaveAttribute('src', mockProps.sprites);
+    expect(screen.getByAltText('pikachu')).toHaveAttribute(
+      'src',
+      mockProps.sprites
+    );
     expect(screen.getByText(mockProps.name)).toBeInTheDocument();
   });
 });
--- test/main.test.tsx
+++ test/main.test.tsx
@@ -1,15 +1,15 @@
 import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import {App} from '../src/app/App';
+import { App } from '../src/app/App';
 import '@testing-library/jest-dom';
 
 describe('main file', () => {
   test('render main', () => {
     render(
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     );
     expect(screen.getByText('2024')).toBeInTheDocument();
     expect(screen.getByText('Search')).toBeInTheDocument();
